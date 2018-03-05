20eb4c51b854994518fb2443c9cc81f4c4fe3740
Matthew <matthewmarshdev@gmail.com^C>
2018-03-05 16:38:43 -0700

    first push, 4:38P 3/5/18

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+require('dotenv').config();
+
+var app = express();
+var port = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "task_saver_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
