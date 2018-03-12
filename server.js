ff7bd17fbf8b919b9f4adc7019a464c8ac98c2ba
Matthew <matthewmarshdev@gmail.com^C>
2018-03-11 21:35:44 -0600

    late nite push

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var method_override = require("method-override");
+var PORT = process.env.PORT || 7000;
+
 require('dotenv').config();
 
 var app = express();
-var port = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
+var hbs = require("handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
+
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "task_saver_db"
-});
+var PORT = process.env.PORT || 7000;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
 
-  console.log("connected as id " + connection.threadId);
-});
+
+
+console.log("Connected at port: " + PORT);
